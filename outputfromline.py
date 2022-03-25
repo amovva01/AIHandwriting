a1b12497a4dae3bb6b30250285312a3330886857
Aneesh <anemovva@gmail.com>
2022-03-24 22:24:40 -0600

    AHHHHHHH I'm to lazy to write a descriptive commit message gl

--- outputfromline.py
+++ outputfromline.py
@@ -1,20 +1,40 @@
-from data import IAMDataset
-from model import HTR
-from fastai.vision.all import show_image
 from PIL import Image
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import numpy as np
+import cv2
 
+def get_text(inputs):
+    pixel_values = processor(inputs, return_tensors="pt").pixel_values
 
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    output = generated_text
+
+    return(output)
+
+def extract_lines(path):
+    img = cv2.imread(path)
+    img2 = cv2.imread(path)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        rect = cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+
+        cropped = img2[y:y + h, x:x + w]
+        cv2.imshow('image', cropped)
+        cv2.waitKey(50)
+        print(get_text(cropped))
 
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten") 
 model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
 inputs = Image.open("imagetoread.png").convert("RGB")
 
-pixel_values = processor(inputs, return_tensors="pt").pixel_values
-
-generated_ids = model.generate(pixel_values)
-generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-output = generated_text
-
+extract_lines("imagetoread.png")
